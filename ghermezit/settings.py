de18f14f86ec15afececf4f1cc84f5564015ebbc
Ali Marasizadeh <marasiali@gmail.com>
2022-04-26 20:18:47 +0430

    add drf spectacular (for swagger api documentaion)

--- ghermezit/settings.py
+++ ghermezit/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'drf_spectacular',
+
     'mainapp',
     'rest_framework',
 ]
@@ -123,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- ghermezit/urls.py
+++ ghermezit/urls.py
@@ -20,6 +20,9 @@ from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/', include('mainapp.api_urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
