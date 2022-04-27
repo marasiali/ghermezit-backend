890fddd40a5c47d67d4ea8d64649e4d237de5ac5
Ali Marasizadeh <marasiali@gmail.com>
2022-04-27 18:43:15 +0430

    add like status to post and comment

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -3,10 +3,24 @@ from .models import Post, Comment, PostReaction, CommentReaction
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
-    likes = serializers.IntegerField()
+    like_status = serializers.SerializerMethodField()
+    likes = serializers.ReadOnlyField()
+
+    def get_like_status(self, obj):
+        if self.context['request'].user.is_authenticated:
+            try:
+                react_obj = obj.postreaction_set.get(author=self.context['request'].user)
+                if react_obj.isLike:
+                    return 1
+                else:
+                    return -1
+            except PostReaction.DoesNotExist:
+                return 0
+        return None
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'author', 'created_at', 'likes']
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'like_status', 'likes']
 
 class PostReactionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,10 +35,24 @@ class CommentCreateSerializer(serializers.ModelSerializer):
 
 class CommentRetrieveSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
-    likes = serializers.IntegerField()
+    like_status = serializers.SerializerMethodField()
+    likes = serializers.ReadOnlyField()
+
+    def get_like_status(self, obj):
+        if self.context['request'].user.is_authenticated:
+            try:
+                react_obj = obj.commentreaction_set.get(author=self.context['request'].user)
+                if react_obj.isLike:
+                    return 1
+                else:
+                    return -1
+            except CommentReaction.DoesNotExist:
+                return 0
+        return None
+    
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'author', 'post', 'created_at', 'likes']
+        fields = ['id', 'content', 'author', 'post', 'created_at', 'like_status', 'likes']
 
 
 class CommentReactionSerializer(serializers.ModelSerializer):
